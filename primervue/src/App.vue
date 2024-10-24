e3a848eb51fd0931860134207f4255dbe8fa4bca
Oscargm13 <oskitargm5@gmail.com>
2024-10-24 14:16:52 +0200

    vue24/10

---
+++ primervue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <HolaMundo />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import HolaMundo from "./components/HolaMundo.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    HolaMundo,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ primervue/src/components/HolaMundo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Mi primera app de Vue</h1>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "HolaMundo"
+}
+</script>
+
+<style>
+    h1 {color: green;}
+</style>
